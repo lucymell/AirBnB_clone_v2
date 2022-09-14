ad86b2806f19fa4990e3d7c650c034768239318e
lucymell <lucy.mutuota@gmail.com>
2022-09-14 15:58:21 +0300

    added files

---
+++ models/amenity.py
@@ -0,0 +1,60 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
+=======
+#!/usr/bin/python3
+"""This is the amenity class"""
+=======
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+>>>>>>> 13ae44e34adcd1a9cff515b78abe3b09012cd9e5
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+<<<<<<< HEAD
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
+>>>>>>> ecfdd0fae2c9bd166d619db51badb42eca854fe7
+=======
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
+>>>>>>> 13ae44e34adcd1a9cff515b78abe3b09012cd9e5
---
+++ models/base_model.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python2
+"""This is the base model class for AirBnB"""
+import uuid
+import models
+import datetime
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """This class will defines all common attributes/methods
+    for other classes
+    """
+    id = Column(String(60),
+                unique=True,
+                nullable=False,
+                primary_key=True)
+
+    created_at = Column(DateTime,
+                        default=datetime.datetime.utcnow(),
+                        nullable=False)
+
+    updated_at = Column(DateTime,
+                        default=datetime.datetime.utcnow(),
+                        nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+                self.created_at = self.updated_at = datetime.datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """return a string representaion
+        """
+        return self.__str__()
+
+    def save(self):
+        """updates the public instance attribute updated_at to current
+        """
+        self.updated_at = datetime.datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
+        """
+        my_dict = dict(self.__dict__)
+        if "_sa_instance_state" in my_dict.keys():
+            del my_dict["_sa_instance_state"]
+        # except KeyError:
+            # pass
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+
+        return my_dict
+
+    def delete(self):
+        """ Ohhhh hohohh that hurt
+        - Jen. (this is method that deletes btw)
+        """
+        models.storage.delete(self)
